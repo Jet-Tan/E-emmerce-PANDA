371ac3d726815459225dc108fb5e8fd3be192d4b
unknown <truongngoctan2388@gmail.com>
2024-12-31 10:02:15 +0700

    commit

--- client/src/pages/ProductAdmin.jsx
+++ client/src/pages/ProductAdmin.jsx
@@ -78,7 +78,8 @@ const ProductAdmin = () => {
   return (
     <section className="">
       <div className="p-2  bg-white shadow-md flex items-center justify-between gap-4">
-        <h2 className="font-semibold">Product</h2>
+        <h1 className="text-2xl font-semibold">Product</h1>
+
         <div className="h-full min-w-24 max-w-56 w-full ml-auto bg-blue-50 px-4 flex items-center gap-3 py-2 rounded  border focus-within:border-primary-200">
           <IoSearchOutline size={25} />
           <input
@@ -92,7 +93,7 @@ const ProductAdmin = () => {
       </div>
       {loading && <Loading />}
 
-      <div className="p-4 bg-blue-50">
+      <div className="p-4 min-h-screen bg-gray-100">
         <div className="min-h-[55vh]">
           <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
             {productData.map((p, index) => {
--- client/src/pages/SubCategoryPage.jsx
+++ client/src/pages/SubCategoryPage.jsx
@@ -142,7 +142,7 @@ const SubCategoryPage = () => {
   return (
     <section className="">
       <div className="p-2   bg-white shadow-md flex items-center justify-between">
-        <h2 className="font-semibold">Sub Category</h2>
+        <h1 className="text-2xl font-semibold">Sub Category</h1>
         <button
           onClick={() => setOpenAddSubCategory(true)}
           className="text-sm border border-primary-200 hover:bg-primary-200 px-3 py-1 rounded"
